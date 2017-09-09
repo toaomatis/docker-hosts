b932925e148242aa97f8e1f86c4c35c45c5164db
Mathijs de Kruyf <mathijs.de.kruyf@axon.tv>
2017-09-09 10:33:42 +0200

    #1 Print container ip in hosts format based on container events

--- docker-hosts.py
+++ docker-hosts.py
@@ -3,8 +3,9 @@ import docker
 client = docker.from_env()
 
 for event in client.events(filters={'type': 'container'}):
-    print(event)
+    print("####")
     for container in client.containers.list():
-        #print(container)
-        host_ip = "{0}\t{1}".format(container.name, container.attrs['NetworkSettings']['IPAddress'])
-        print(host_ip)
+        print("# {0}".format(container.name))
+        for network in container.attrs['NetworkSettings']['Networks']:
+            ip_addresses = "{1} {0}.docker".format(container.name, container.attrs['NetworkSettings']['Networks'][network]['IPAddress'])
+            print(ip_addresses)
