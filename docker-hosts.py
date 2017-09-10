df649ca1b4bab043258dc5d284ba734963f28e35
Mathijs de Kruyf <mathijs.de.kruyf@axon.tv>
2017-09-10 10:37:33 +0200

    Try for socket errors, remove lines on close

--- docker-hosts.py
+++ docker-hosts.py
@@ -9,6 +9,7 @@ client = docker.from_env()
 
 
 def remove_lines():
+    print(remove_lines.__name__)
     file = open(HOSTS_FILE, "r+")
     lines = file.readlines()
     file.seek(0)
@@ -29,6 +30,7 @@ def remove_lines():
 
 
 def write_lines(lines):
+    print(write_lines.__name__)
     file = open(HOSTS_FILE, "a")
     file.write(DOCKER_HOSTS_START)
     file.writelines(lines)
@@ -37,15 +39,31 @@ def write_lines(lines):
     return
 
 
-remove_lines()
-
-for event in client.events(filters={'type': 'container'}):
+def update_ips():
+    print(update_ips.__name__)
     remove_lines()
     lines = []
     for container in client.containers.list():
         print("# {0}".format(container.name))
         for network in container.attrs['NetworkSettings']['Networks']:
-            ip_addresses = "{1} {0}.docker".format(container.name, container.attrs['NetworkSettings']['Networks'][network]['IPAddress'])
+            ip_addresses = "{1} {0}.docker".format(container.name,
+                                                   container.attrs['NetworkSettings']['Networks'][network]['IPAddress'])
             print(ip_addresses)
-            lines.append(ip_addresses+NL)
+            lines.append(ip_addresses + NL)
     write_lines(lines)
+    return
+
+
+try:
+    print("try")
+    update_ips()
+    for event in client.events(filters={'type': 'container'}):
+        update_ips()
+
+except Exception as e:
+    print("except")
+    print(e)
+
+finally:
+    print("finally")
+    remove_lines()
