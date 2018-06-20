b75a03f650da63f7b4a19e6c727f28ab8234aff3
Mathijs de Kruyf <mathijs.dekruyf@dare.it>
2018-06-20 08:22:21 +0200

    Initial install script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+function pretty_print {
+  echo -e "${GREEN}$1${NC}"
+}
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+DIR=$(dirname $(readlink -f $0))
+DIR_USER=$(stat -c '%U' $DIR)
+VENV_DIR="$DIR/env"
+SERVICE_DIR='/etc/systemd/system'
+
+if [[ $EUID -eq 0 ]]; then
+   pretty_print "${RED}This script must not be run as root!$NC"
+   pretty_print "Run as $RED$DIR_USER$GREEN instead."
+   pretty_print "You'll be prompted when elevated privileges are necessary."
+   exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+pretty_print "Installing dependencies"
+sudo apt --yes install python3 python3-pip
+
+pretty_print "Installing Docker Hosts from $RED$DIR$NC"
+pip3 install virtualenv
+
+pretty_print "Creating a virtualenv in $RED$VENV_DIR$NC"
+python3 -m venv $VENV_DIR
+
+pretty_print "Activating the virtualenv"
+source env/bin/activate
+pretty_print "Install docker inside virtualenv"
+pip install docker
+deactivate
+
+pretty_print "Adapt absolute paths in sources"
+sed -i -e "s|/home/mathijs/git/docker/docker-hosts|$(pwd)|g" $(grep -Irl /home/mathijs/git/docker/docker-hosts)
+
+pretty_print "Copy service files to $RED$SERVICE_DIR"
+sudo cp docker-hosts.service $SERVICE_DIR
+pretty_print "Install and enable the service"
+sudo systemctl daemon-reload
+sudo systemctl start docker-hosts.service
+sudo systemctl enable docker-hosts.service
+
+pretty_print "Verify if ${RED}docker-hosts.service${GREEN} is running"
+systemctl status docker-hosts.service
